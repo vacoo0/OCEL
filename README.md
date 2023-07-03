2ce22266f6b92806798c107cf7bf078ac9cce39c
MSpinczyk <michal.spinczyk@gmail.com>
2023-07-03 22:20:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Creating object-centric event logs from instructional/tutorial/how to videos
+
+A brief description of your project goes here.
+
+## Table of Contents
+
+- [Project Description](#project-description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Project Description
+
+The aim of this project is to develop a system that can automatically generate object-centric event logs from instructional, tutorial, or how-to videos. In today's digital age, there is an abundance of video content available online, providing step-by-step instructions for various tasks and activities. However, it can be challenging to extract structured data from these videos to create event logs that capture the sequence of actions performed on specific objects.
+
+By leveraging computer vision techniques and natural language processing, this project aims to bridge the gap between unstructured video content and structured event logs. The system will analyze the video frames to detect and track objects of interest, such as tools, equipment, or items being manipulated by the instructor in the video. It will also process the accompanying transcriptions to extract relevant textual instructions or annotations.
+
+The extracted information will be used to construct object-centric event logs, which will capture the temporal order of actions performed on each object. These event logs can be invaluable for various applications, including process automation, knowledge extraction, or interactive tutorials.
+
+## Installation
+
+Provide step-by-step instructions on how to install your project locally. Include any prerequisites or dependencies required to run the project.
+
+## Usage
+
+Explain how to use your project. Provide examples and code snippets if necessary. Include any configuration options or environment variables that need to be set.
+
+## Features
+
+List the key features of your project here. Highlight the main functionalities or components that make your project stand out.
+
+## Contributing
+
+Explain how others can contribute to your project. Provide guidelines for submitting bug reports, feature requests, or pull requests. Include a code of conduct if applicable.
+
+## License
+
+Specify the license under which your project is distributed. Include any relevant copyright or attribution notices.
+
+## Acknowledgements
+
+If your project relies on or was inspired by other works, mention them here. Provide links or references to any external resources that were helpful in creating your project.
+
+## Contact
+
+Provide your contact information or links to your social media profiles. Encourage users to reach out with questions, feedback, or suggestions.
---
+++ events.py
@@ -0,0 +1,76 @@
+# pm4py.convert.convert_log_to_ocel(log: Union[EventLog, EventStream, DataFrame], activity_column: str = 'concept:name', timestamp_column: str = 'time:timestamp', 
+#                                   object_types: Optional[Collection[str]] = None, 
+#                                   obj_separator: str = ' AND ', additional_event_attributes: Optional[Collection[str]] = None) → OCEL
+
+# pm4py.convert.convert_log_to_ocel
+import datetime
+import pm4py
+import pandas as pd
+
+# log = pm4py.write_ocel2_sqlite(ocel, '<path_to_export_to>')
+
+class Event:
+    def __init__(self, event_id, timestamp, activity, duration, summary, items_text, items_image) -> None:
+        self.event_id = event_id
+        self.timestamp = timestamp
+        self.activity = activity
+        self.duration = duration
+        self.summary = summary
+        self.items_text = items_text
+        self.items_image = items_image
+        
+
+class EventLog:
+    def __init__(self):
+        self.events = []
+    
+    def add_event(self, event: Event):
+        self.events.append(event)
+    
+    def remove_event(self, event_id):
+        self.events = [event for event in self.events if event.event_id != event_id]
+     
+    def get_events(self):
+        return self.events
+    
+    def create_dataframe(self):
+        event_ids = []
+        timestamps = []
+        activities = []
+        durations = []
+        summaries = []
+        items_texts = []
+        items_images = []
+        for ev in self.events:
+            event_ids.append(ev.event_id)
+            timestamps.append(ev.timestamp)
+            activities.append(ev.activity)
+            durations.append(ev.duration)
+            summaries.append(ev.summary)
+            items_texts.append(ev.items_text)
+            items_images.append(ev.items_image)
+        df = pd.DataFrame(data={'ocel:eid':event_ids , 'time:timestamp':timestamps, 'ocel:activity':activities, 'duration':durations, 'summary':summaries, 
+                       'ocel:type:items_text':items_texts, 'ocel:type:items_image':items_images})
+        return df
+    
+    def create_ocel(self):
+        # pm4py.convert.convert_log_to_ocel(log: Union[EventLog, EventStream, DataFrame], activity_column: str = 'concept:name', timestamp_column: str = 'time:timestamp', 
+#                                   object_types: Optional[Collection[str]] = None, 
+#                                   obj_separator: str = ' AND ', additional_event_attributes: Optional[Collection[str]] = None) → OCEL
+        return pm4py.convert.convert_log_to_ocel(log = self.create_dataframe(), activity_column = 'ocel:activity', timestamp_column = 'time:timestamp', 
+                                    object_types = ['ocel:type:items_text', 'ocel:type:items_image'], 
+                                    obj_separator = ' AND ', additional_event_attributes = ['ocel:eid','duration','summary'])
+        
+
+    def save_OCEL_standard(self):
+        pm4py.write.write_ocel_csv(self.create_ocel(), 'logs/events/ocel_test.csv', 'logs/objects/ocel_test.csv')
+
+ev = Event(1,'4.56','making',8,'good text', 'text2', 'text1')
+
+log = EventLog()
+log.add_event(ev)
+obj = log.create_ocel()
+print(obj)
+print(obj.get_extended_table())
+log.save_OCEL_standard()
+# print(log.save_OCEL_standard())

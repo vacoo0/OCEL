fbebcaa70f115967fe858b55bb378d4cb946df52
Pudro <ptr.pustelnik@gmail.com>
2023-07-05 23:42:24 +0200

    added downloader class

--- download_video_ydl.py
+++ download_video_ydl.py
@@ -1,7 +1,34 @@
 import yt_dlp as youtube_dl
 
-ydl_opts = {
-    'format': 'bestvideo[height<=720][ext=mp4]'
-}
-ydl = youtube_dl.YoutubeDL(ydl_opts)
-ydl.download(['https://www.youtube.com/watch?v=iNBTSDryewM'])
+class YouTubeDownloader:
+    def __init__(self, save_directory, resolution='360', format='mp4', framerate=None):
+        self.save_directory = save_directory
+        self.resolution = resolution
+        self.format = format
+        self.framerate = framerate
+    
+    def download_video(self, video_url):
+        ydl_opts = {
+            'format': self._build_format_string(),
+            'outtmpl': self._build_output_template(),
+        }
+        
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([video_url])
+    
+    def _build_format_string(self):
+        format_string = f'bestvideo[height<={self.resolution}][ext={self.format}]+bestaudio[ext={self.format}]/best[height<={self.resolution}][ext={self.format}]'
+        
+        if self.framerate:
+            format_string += f'/best[height<={self.resolution}][ext={self.format}][fps={self.framerate}]'
+        
+        return format_string
+    
+    def _build_output_template(self):
+        return f'{self.save_directory}/%(title)s.%(ext)s'
+
+# Usage example
+if __name__ == '__main__':
+    downloader = YouTubeDownloader(save_directory='./videos', resolution='360', format='mp4', framerate=None)
+    video_url = 'https://www.youtube.com/watch?v=iNBTSDryewM'
+    downloader.download_video(video_url)
